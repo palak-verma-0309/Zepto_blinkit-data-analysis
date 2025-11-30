b58e9d94648719c1640bf205198fec734734343b
palak-verma-0309 <palakv0309@gmail.com>
2025-11-30 15:57:11 +0530

    ml updated

--- ml.py
+++ ml.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("grocery_orders.csv")
 df.columns = df.columns.str.strip()
@@ -42,6 +43,14 @@ model = Pipeline([
     ("rf", RandomForestClassifier(n_estimators=100, random_state=42))
 ])
 
-model.fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+print("Model Accuracy:", acc)
 
 joblib.dump(model, "blinkit_zepto_model.pkl")
